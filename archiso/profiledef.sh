8d90b7a6c8bbfefd69db381b195e0ce9f52848c6
TechXero <steve@techxero.com>
2021-05-15 11:51:14 +0300

    Archiso updated files in

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -7,6 +7,7 @@ iso_publisher="XeroLinux <https://techxero.github.io>"
 iso_application="XeroLinux Live/Rescue CD"
 iso_version="$(date +%Y.%m.%d)"
 install_dir="arch"
+buildmodes=('iso')
 bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
