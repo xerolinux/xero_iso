149b60b2ebdc2e4c8232e573eec1a35276aec16c
XeroLinux <steve@techxero.com>
2022-05-30 13:43:32 +0300

    Upgrade

--- archiso/profiledef.sh
+++ archiso/profiledef.sh
@@ -8,7 +8,8 @@ iso_application="XeroLinux Live/Rescue CD"
 iso_version="main"
 install_dir="arch"
 buildmodes=('iso')
-bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito' 'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-x64.systemd-boot.esp' 'uefi-x64.systemd-boot.eltorito')
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="squashfs"
--- build-scripts/build.sh
+++ build-scripts/build.sh
@@ -24,7 +24,7 @@ echo
 	#isoLabel='arcolinux-next-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 63-2"
+	archisoRequiredVersion="archiso 64-1"
 	buildFolder=$HOME"/xero-build"
 	outFolder=$HOME"/xero-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
